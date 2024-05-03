f8e3e4a07330873ce3d51a8dc9a2e96d4c9a8447
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-05-03 18:31:48 +0300

    ©BULLSEYE

--- plugins/GPT4.js
+++ plugins/GPT4.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
        
   } catch (error) {
     console.error(error);
-    m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
+    m.reply('Oops! Something went wrong. , we are trying had to fix it asap 😅');
   }
 }
 handler.help = ['gemini <text>']
