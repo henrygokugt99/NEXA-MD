ba5201d5c2708e9908f900051a8f72c34ef6ec64
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-05-03 13:19:35 +0300

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 
-╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
-│ 📂 *BOT Name:* _XLICON-V2-MD_
-│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
-│ 👤 *Owner:* _Salman Ahmad_
-│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+╭⭑⭑⭑★✪ *BULLSEYE* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _BULLSEYE-BM-MD_
+│ 📝 *Description:* _I'm BULLSEYE-MD. A MultiDevice WhatsApp bot with rich features Created By NEXA TEAM and Ashley kakashi._
+│ 👤 *Owner:* _NEXA 256_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaWeUY9Jf05hxbJkVW23
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['channel']
