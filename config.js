92221cbf8effed2ab888e4a5160f77f179bc9ba2
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-05-05 20:08:30 +0300

    © BULLSEYE

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['+256789810043', '₦ɆӾ₳▂ ▄ ▅ ▆ ▇ █', true], 
+  ['256789810043', 'NEXA AI', true], 
   ['+263780129060', 'Ashley kakashi', true],
   ['2348026018074', 'David develop', true], 
-  ['+256708316668', 'off_street_256', true],
+  ['256708316668', 'off_streets', true],
   ['256701507880', 'NEXA 2', true],
-  ['+918360234087', 'watch your back', true],
+  ['918360234087', 'white 4', true],
   
 ] //Number of owners
 
