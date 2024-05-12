34305a10520e956c8625577decc5668ed2726754
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-05-12 14:27:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,26 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-
-global.owner = [ 
-  ['+263780129060', 'Ashley kakashi', true],
-  ['2348026018074', 'David develop', true], 
-  ['256701507880', 'NEXA 2', true],
-  ['233508488192', 'AB  Dev', true]
-  
+global.owner = [
+  ['256789810043', 'NEXA', true], 
+  ['233208179431', 'Abraham Dwamena', true],
+  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['2348026018074', 'David Dev ⚡', true],
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
-
-global.mods = ['923184070915', '94789481495', '923184474176','256789810043','233208179431'] 
-global.prems = ['923184070915','256708316668','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '256789810043', '94789481495', '918360234087','233531910067']
+global.mods = ['923184070915', '256789810043', '923184474176', '233531910067', '233508488192'] 
+global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.allowed = ['923184070915', '256789810043', '94789481495', '918360234087', '233508488192']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 global.beta = 'mLxstUwm'
 
 global.APIs = { // API Prefix
-  // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
@@ -42,13 +37,13 @@ global.APIs = { // API Prefix
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net'
 }
+
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.neoxr.my.id': global.keysneoxr,
   'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://zenzapis.xyz': global.keysxxx, 
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
@@ -57,7 +52,7 @@ global.botname = 'BULLSEYE-MD'
 global.premium = 'true'
 global.packname = 'NEXA TEAM' 
 global.author = '@BULLSEYE-MD' 
-global.menuvid = 'https://telegra.ph/file/d6c3912b87f8affcb8733.mp4'
+global.menuvid = 'https://i.imgur.com/1wXJgrV.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaWeUY9Jf05hxbJkVW23\n' 
 global.dygp = 'https://chat.whatsapp.com/LpacbNXvuhqLAr7AZwyU3r'
 global.fgsc = 'https://github.com/henrygokugt99/NEXA-MD' 
@@ -66,16 +61,15 @@ global.fgpyp = 'https://youtube.com/@henrygokugt256'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
-
-global.wait = '*🏂 _BULLSEYE IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
-global.rwait = '📜'
-global.dmoji = '🌝'
+global.wait = '*🏂 _NEXA IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
 global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
+global.maxwarn = '3' // maximum warnings
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
