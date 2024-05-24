22c95f11535aa7de3c7154719682ad9c915ce476
NEXA goat <144521464+henrygokugt99@users.noreply.github.com>
2024-05-24 16:04:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,17 +6,13 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233508488192', 'Abraham Dwamena3', true]
+  ['256789810043', 'NEXA', true], 
+  ['2348026018074', 'DAVID EALS', true], 
 ] //Number of owners
 
 global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.prems = ['923184070915', '256789810043', '94789481495', '918360234087', '233508488192']
+global.allowed = ['923184070915', '2348026018074', '94789481495', '918360234087', '233508488192']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,23 +46,23 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'BULLSEYE-MD'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'NEXA TEAM' 
 global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
+global.menuvid = 'https://i.imgur.com/afVHkUD.mp4'
+global.igfg = '▢✓ Follow Our channel\nhttps://chat.whatsapp.com/LpacbNXvuhqLAr7AZwyU3r\n' 
+global.dygp = 'https://chat.whatsapp.com/LpacbNXvuhqLAr7AZwyU3r'
+global.fgsc = 'https://github.com/henrygokugt99/NEXA-MD' 
+global.fgyt = 'https://youtube.com/@henrygokugt256'
+global.fgpyp = 'https://youtube.com/@henrygokugt256'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
-global.wait = '*🕣 _XLICON IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✔️'
+global.wait = '*⏲️ _NEXA IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.rwait = '📜'
+global.dmoji = '👀'
+global.done = '✅'
 global.error = '❌' 
 global.xmoji = '🔥' 
 
